59ed97d5132fce9597a28092be7eaa3a3db6465f
1763124707@qq.com <1763124707@qq.com>
2019-12-07 18:05:16 +0800

    springcloud-demo:添加两个模块springcloud-tcc-bank1、springcloud-tcc-bank2 基于使用seata的tcc方式之一实现分布式的事务。

---
+++ springcloud-tcc-bank1/src/main/java/com/example/tccbank1/SpringcloudTccBank1Application.java
@@ -0,0 +1,13 @@
+package com.example.tccbank1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudTccBank1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudTccBank1Application.class, args);
+    }
+
+}
---
+++ springcloud-tcc-bank2/src/main/java/com/example/tccbank2/SpringcloudTccBank2Application.java
@@ -0,0 +1,13 @@
+package com.example.tccbank2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudTccBank2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudTccBank2Application.class, args);
+    }
+
+}
